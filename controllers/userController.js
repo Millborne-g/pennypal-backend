0b30e986dc9b567be9d35233c5b131146d4720d5
Millborne <millborne.galamiton@supportzebra.com>
2024-06-13 15:30:57 +0800

    Add login to controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,46 +1,80 @@
 // controllers/userController.js
 
-const User = require('../models/userModel');
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.JWTSECRET, { expiresIn: "2m" });
+};
 
 // Controller to register a new user
 exports.getAllUsers = async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (error) {
-    console.error(error.code);
-    res.status(500).json({ message: 'Internal Server Error', code: error.code });
-  }
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        console.error(error.code);
+        res.status(500).json({
+            message: "Internal Server Error",
+            code: error.code,
+        });
+    }
 };
 
 // Controller to register a new user
 exports.registerUser = async (req, res) => {
-  const { email, password, fullName, firstName, lastName, userImage } = req.body;
-  try {
-    const newUser = new User({ email, password, fullName, firstName, lastName, userImage });
-    const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
-  } catch (error) {
-    console.error(error.code);
-    res.status(500).json({ message: 'Internal Server Error', code: error.code });
-  }
+    const { email, password, fullName, firstName, lastName, userImage } =
+        req.body;
+    try {
+        const newUser = new User({
+            email,
+            password,
+            fullName,
+            firstName,
+            lastName,
+            userImage,
+        });
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (error) {
+        console.error(error.code);
+        res.status(500).json({
+            message: "Internal Server Error",
+            code: error.code,
+        });
+    }
+};
+
+exports.loginUser = async (req, res) => {
+    const { email, password } = req.params;
+
+    try {
+        const user = await User.findOne({ email, password });
+        if (!user) {
+            res.status(404).json({ message: "user not found!" });
+        }
+        // create a token
+        const token = createToken(user._id);
+        res.json({ user, token: token });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 };
 
 exports.findUserByEmail = async (req, res) => {
-  const { email } = req.params;
+    const { email } = req.params;
 
-  try {
-    const user = await User.findOne({ email });
+    try {
+        const user = await User.findOne({ email });
 
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+        if (!user) {
+            return res.status(404).json({ message: "User not found!" });
+        }
+
+        res.json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
     }
-
-    res.json(user);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
 };
-
-
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,6 +10,9 @@ router.post('/user/register', userController.registerUser);
 //Get all users
 router.get('/user/users', userController.getAllUsers);
 
+// Login user
+router.get("/user/login/:email/:password", userController.loginUser);
+
 // Find a user by email
 router.get('/users/:email', userController.findUserByEmail);
 
